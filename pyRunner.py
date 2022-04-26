9bf87389317a0835b8489f7f70309b3b444e4875
홍유준 <hyjhyj0901@pusan.ac.kr>
2022-04-26 23:16:24 +0900

    use abs path now

--- pyRunner.py
+++ pyRunner.py
@@ -2,7 +2,24 @@ import subprocess as sp
 import sys
 import os
 import shutil
- 
+
+base_dir = '/Users/yoojunhong/Desktop/'
+file_list = os.listdir(base_dir)
+assign_list = list(filter(lambda str : '컴퓨터및프로그래밍입문' in str, file_list))
+
+print ("Select the directory to excute")
+for i in range(len(assign_list)):
+    print(i, ':', assign_list[i][42::])
+
+curr_dir = base_dir
+
+while True:
+    try:
+        curr_dir += assign_list[int(input())]
+        break
+    except IndexError as e:
+        print("Invalid Number!")
+
 testcase = ['' for i in range(10)]
 # testcase[0] = ''
 # testcase[1] = ''
@@ -10,44 +27,38 @@ testcase = ['' for i in range(10)]
 # testcase[3] = ''
 # testcase[4] = ''
 
-if os.path.exists("_Results"):
-    shutil.rmtree("_Results")
-os.makedirs("_Results")
+if os.path.exists(curr_dir + "/_Results"): 
+    shutil.rmtree(curr_dir + "/_Results")
+os.makedirs(curr_dir + "/_Results")
 
-dirs = sp.run(args=['ls -d *assignsubmission_file_/'], shell=True, text=True, capture_output=True, encoding='utf-8')
-dirNameList = dirs.stdout.split('\n')
+dirNameList = list(filter(lambda str : str.endswith("assignsubmission_file_") , os.listdir(curr_dir)))
 
 for dirName in dirNameList:
     if len(dirName) == 0 : continue
     studentName = dirName.split('-')[0]
     print("⇩ ", studentName)
 
-    pyfiles = sp.run(args=['ls '+ dirName.replace(' ', '\ ') + '*.py'], shell=True, text=True, capture_output=True, encoding='utf-8') \
-                .stdout  \
-                .strip() \
-                .split('\n') \
-
+    pyfiles = list(filter(lambda str : str.endswith(".py"), os.listdir(curr_dir + '/' + dirName)))
     flag = False
 
     for idx, fileName in enumerate(pyfiles):
-        if len(fileName)==0:
-            continue
-        print("  >", fileName.split('/')[1])
+        if len(fileName)==0: continue
+        print(" >", fileName)
         flag = True
- 
-        with open('./_Results/0'+str(idx+1)+'_'+studentName+'.txt', "a", encoding='utf-8') as result:
+
+        with open(f'{curr_dir}/_Results/0{str(idx+1)}_{studentName}.txt', "w", encoding='utf-8') as result:
+            filePath = f'{curr_dir}/{dirName}/{fileName}'
+            
             result.write('\n######## OUTPUT ########\n\n')
-            sp.run(["chmod", "a+x", fileName])
-            out = sp.run(args=[sys.executable, fileName], text=True, capture_output=True, input=testcase[idx])
+            sp.run(["chmod", "a+x", filePath])
+            out = sp.run(args=[sys.executable, filePath], text=True, capture_output=True, input=testcase[idx])
             result.write(out.stdout)
 
-            pycode = open('./' +fileName, 'r', encoding='utf-8')
-            result.write('\n\n###### '+fileName.split('/')[1]+' ######\n\n')
+            pycode = open(filePath, 'r', encoding='utf-8')
+            result.write('\n\n###### '+fileName+' ######\n\n')
             result.write(pycode.read())
             pycode.close()
-        result.close()
-    print()
 
     if(flag == False):
-        wrong = open('./_Results/ERR_'+studentName, "w", encoding='utf-8')
+        wrong = open(f'{curr_dir}/_Results/ERR_{studentName}', "w", encoding='utf-8')
         wrong.close()
